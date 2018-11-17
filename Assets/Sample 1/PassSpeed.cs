fadf814f005832de5bbb4f091e0d5c07baa417bf
adLucem <lois.aubree@gmail.com>
2018-11-17 23:16:57 +0100

    INIT + UPDATE Samples 1 - 2 - 3

---
+++ Assets/Sample 1/PassSpeed.cs
@@ -0,0 +1,26 @@
+﻿
+using UnityEngine;
+using UnityEngine.Experimental.VFX;
+
+public class PassSpeed : MonoBehaviour
+{
+    [SerializeField] Lasp.FilterType _filterType;
+
+    [SerializeField] private VisualEffect _visualEffect;
+
+    [SerializeField] float _amplify;
+    public float Amplify
+    {
+        get { return _amplify; }
+        set { _amplify = value; }
+    }
+
+    void Update()
+    {
+        var rms = Lasp.AudioInput.CalculateRMSDecibel(_filterType) + _amplify;
+        var level = 1 + rms * 0.1f;
+        level = Mathf.Clamp01(level);
+
+        _visualEffect.SetFloat(Shader.PropertyToID("LowPassSpeed"), level);
+    }
+}
